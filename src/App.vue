05f5fe95b3c0e2618b99b63c06504b3ec0fec111
Theja Rasanjalee <ugthejaugamage@gmail.com>
2020-11-18 09:13:21 +0530

    card v1

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <main id="app" >
+  <section class="items">
+<Item
+v-for="Item in products" 
+:key="Item.color"
+:Item="Item" />
+  </section>
+  </main>
+</template>
+
+<script>
+import Item from './components/Item.vue'
+
+export default {
+  name: 'App',
+  components:{
+   Item
+   
+  },
+  data(){
+    return{
+      products:[
+        {
+          title: 'Nike Air Max',
+          color: 'green',
+          bgtext: 'NIKE',
+          src: require('./assets/green-shoe.png')
+        },
+        {
+          title: 'Nike flex',
+          color: 'blue',
+          bgtext: 'AIR',
+          src: require('./assets/blue-shoe.png')
+        },
+        {
+          title: 'Nike Roche Runs',
+          color: 'pink',
+          bgtext: 'MAX',
+          src: require('./assets/pink-shoe.png')
+        }
+      ]
+
+      
+    }
+  }
+  
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'montseratt', sans-serif;
+  }
+
+ main {
+    width: 100vw;
+    min-height: 100vh;
+    overflow: hidden;
+    background-color: #EEE;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+ .items {
+    display: flex;
+    max-width: 1280px;
+    padding: 25px;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="items">
+      <div class="item-inner">
+          <div class="item-text-wrap">
+              <h2 class="bg-text">{{Item.bgtext}}</h2>
+          </div>
+<div class="item-image-wrap">
+    <img :src="Item.src" alt="tr dev" class="image"/>
+</div>
+<div class="item-detail">
+    <h2>{{Item.title}}</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci vero cumque, velit deleniti voluptatum animi praesentium rerum, id natus expedita earum, quisquam veritatis suscipit hic officia at soluta porro dolores!</p>
+</div>
+          
+      </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Item',
+    props:['Item']
+}
+</script>
+
+<style >
+.items {
+    flex: 1 1 33.333%;
+    width: 100%;
+    padding: 25px;
+  }
+  .item-inner {
+    position: relative;
+    padding: 25px;
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+   
+  }
+ .item-image-wrap .image {
+    width: 100%;
+    filter: drop-shadow(0px 0px 12px rgba(0, 0, 0, 0.25));
+  }
+ 
+</style>
