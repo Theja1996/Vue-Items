944265ad2bde99e9f4aef9f0541237b48ee80c1a
Theja Rasanjalee <ugthejaugamage@gmail.com>
2020-11-19 09:09:27 +0530

    card has responsiveness issue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <main id="app" >
+  <main id="app"  @mousemove="mousemove" >
   <section class="items">
 <Item
-v-for="Item in products" 
-:key="Item.color"
-:Item="Item" />
+v-for="item in items" 
+:key="item.color"
+:item="item" />
   </section>
   </main>
 </template>
@@ -13,14 +13,14 @@ v-for="Item in products"
 import Item from './components/Item.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components:{
    Item
    
   },
   data(){
     return{
-      products:[
+      items:[
         {
           title: 'Nike Air Max',
           color: 'green',
@@ -43,9 +43,35 @@ export default {
 
       
     }
+  },
+   methods: {
+    mousemove (e) {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+      let items = document.querySelectorAll('.items .item');
+      for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        let item_image = item.querySelector('.item-image-wrap');
+        let img_x = mouseX - this.coords(item_image).x;
+        let img_y = mouseY - this.coords(item_image).y;
+        item_image.style.transform = `translateY(-${img_y/40}px) translateX(-${img_x/40}px) translateZ(100px)`;
+       
+       let bgtext = item.querySelector('.bg-text');
+        let bg_x = mouseX - this.coords(bgtext).x;
+        bgtext.style.transform = `translateX(${bg_x/25}px)`;
+      }
+    },
+    coords (el) {
+      let coords = el.getBoundingClientRect();
+      return {
+        x: coords.left / 2,
+        y: coords.top / 2
+      }
+    }
   }
   
 }
+
 </script>
 
 <style>
--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="items">
-      <div class="item-inner">
+      <div :class="'item-inner  ' + item.color">
           <div class="item-text-wrap">
-              <h2 class="bg-text">{{Item.bgtext}}</h2>
+              <h2 class="bg-text">{{item.bgtext}}</h2>
           </div>
 <div class="item-image-wrap">
-    <img :src="Item.src" alt="tr dev" class="image"/>
+    <img :src="item.src" alt="tr dev" class="image"/>
 </div>
 <div class="item-detail">
-    <h2>{{Item.title}}</h2>
+    <h2>{{item.title}}</h2>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci vero cumque, velit deleniti voluptatum animi praesentium rerum, id natus expedita earum, quisquam veritatis suscipit hic officia at soluta porro dolores!</p>
 </div>
           
@@ -20,7 +20,7 @@
 <script>
 export default {
     name:'Item',
-    props:['Item']
+    props:['item']
 }
 </script>
 
@@ -40,5 +40,53 @@ export default {
     width: 100%;
     filter: drop-shadow(0px 0px 12px rgba(0, 0, 0, 0.25));
   }
- 
+
+  .item-inner.green {
+    background-image: linear-gradient(to bottom right, #24D484, #116432);
+  }
+ .item-inner.blue {
+    background-image: linear-gradient(to bottom left, #24D484, #2474C4 70%);
+  }
+  .item-inner.pink {
+    background-image: linear-gradient(to bottom right, #F444A4, #1168D4);
+  }
+  .item-text-wrap {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 0;
+    overflow: hidden;
+    perspective: 1000px;
+  }
+   .item-text-wrap h2 {
+    color: #313131;
+    font-size: 128px;
+    font-weight: 900;
+    opacity: 0.2;
+    transform-origin: center;
+  }
+   .item-image-wrap {
+    position: relative;
+    z-index: 1;
+    transform-origin: center;
+  }
+  .item-detail {
+    background-color: #FFF;
+    padding: 25px;
+    margin: 0px -25px -25px;
+  }
+  .item-detail h2 {
+    font-size: 24px;
+    font-weight: 700;
+    color: #676767;
+    margin-bottom: 15px;
+  }
+  .item-detail p {
+    font-size: 14px;
+    line-height: 1.5;
+    font-weight: 300;
+    color: #676767;
+  }
 </style>
